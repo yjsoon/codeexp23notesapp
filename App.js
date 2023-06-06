2d444bc764483445680a7ec9dc1aff31f321e437
YJ Soon <yjsoon@tinkertanker.com>
2023-06-06 14:23:17 +0800

    Styled the front page, thanks ChatGPT

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
 
-export default function App() {
+function NotesScreen() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -10,11 +12,36 @@ export default function App() {
   );
 }
 
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#f93",
+            height: 130, // Adjust the height as per your requirement
+            elevation: 4, // Adds a shadow for Android devices
+            borderBottomWidth: 1, // Adds a border under the header
+            borderBottomColor: "gray" // Border color
+          },
+          headerTitleStyle: {
+            fontWeight: "bold",
+            fontSize: 30
+          }
+        }}>
+        <Stack.Screen name="Notes" component={NotesScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#ff8",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
