7ce5c244e7ff5322cfae53da3bafca47ca897ea3
YJ Soon <yjsoon@tinkertanker.com>
2023-06-06 15:13:06 +0800

    Styled add note screen

--- components/AddScreen.js
+++ components/AddScreen.js
@@ -1,15 +1,38 @@
-import { Text, View, StyleSheet, Button } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Button,
+  TextInput,
+  TouchableOpacity
+} from "react-native";
+import { useState } from "react";
 
 export default function AddScreen({ navigation }) {
+  const [text, setText] = useState("");
+
   return (
     <View style={styles.container}>
-      <Text>This is the add screen</Text>
-      <Button
-        onPress={() => {
-          navigation.goBack();
+      <Text style={styles.title}>Add a note</Text>
+      <TextInput
+        value={text}
+        style={styles.textInput}
+        onChangeText={(newText) => {
+          setText(newText);
         }}
-        title="Dismiss"
       />
+      <View style={styles.buttonRow}>
+        <TouchableOpacity style={styles.button} onPress={() => {}}>
+          <Text style={styles.buttonText}>Submit</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.button, { backgroundColor: "red" }]}
+          onPress={() => {
+            navigation.goBack();
+          }}>
+          <Text style={styles.buttonText}>Dismiss</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -20,5 +43,29 @@ const styles = StyleSheet.create({
     backgroundColor: "#ffe",
     alignItems: "center",
     justifyContent: "center"
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+    fontWeight: "bold"
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    width: "70%",
+    padding: 5,
+    marginBottom: 20
+  },
+  buttonRow: {
+    flexDirection: "row"
+  },
+  button: {
+    padding: 20,
+    backgroundColor: "orange",
+    margin: 10
+  },
+  buttonText: {
+    fontWeight: "bold",
+    color: "white"
   }
 });
